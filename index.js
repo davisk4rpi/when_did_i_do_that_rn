34d122aa81c76fd449ad1afdc0936d252b0d032e
Kieran Davis <davisk4rpi@gmail.com>
2022-12-28 18:08:39 -0500

    Adds/updates external libs, adds some configuration

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 /**
  * @format
  */
+import 'react-native-gesture-handler';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { AppRegistry } from 'react-native';
-import { en, registerTranslation } from 'react-native-paper-dates';
 import 'react-native-get-random-values';
 
 import App from './App';
 import { name as appName } from './app.json';
 
-registerTranslation('en', en);
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
