32d68551ef2ddc834a306a82a219110116c1ca9e
Kieran Davis <davisk4rpi@gmail.com>
2022-12-28 21:56:47 -0500

    removes screens from the navigation lib and adds switch to toggle light/dark themes

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -2,46 +2,44 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Translation } from '../app-context';
-import { Header } from '../components';
-import { HomeScreen, NewTrackerScreen, TrackerScreen } from './screens';
+import { ThemeToggle, Translation } from '../app-context';
+import { HomeScreen, NewTrackerScreen, TrackerScreen } from '../screens';
 import { RootStackParamList } from './types';
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   return (
-    <RootStack.Navigator
-      screenOptions={{
-        header: props => <Header {...props} />,
-      }}>
+    <RootStack.Navigator screenOptions={{ headerTransparent: true }}>
       <RootStack.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          headerTitle: Translation.whenDidIDoThat,
+          title: Translation.whenDidIDoThat,
+          headerRight: ThemeToggle,
         }}
       />
-      <RootStack.Group screenOptions={{ presentation: 'modal' }}>
-        <RootStack.Screen
-          name="NewTracker"
-          component={NewTrackerScreen}
-          options={{
-            header: undefined,
-            title: Translation.addANewTracker,
-            headerBackVisible: true,
-          }}
-        />
-        <RootStack.Screen
-          name="Tracker"
-          component={TrackerScreen}
-          options={({ route }) => ({
-            header: undefined,
-            headerBackVisible: true,
-            title: route.params.name,
-          })}
-        />
-      </RootStack.Group>
+      {/* <RootStack.Group
+        screenOptions={{
+          presentation: Platform.OS === 'ios' ? 'modal' : 'card',
+        }}> */}
+      <RootStack.Screen
+        name="NewTracker"
+        component={NewTrackerScreen}
+        options={{
+          title: Translation.addANewTracker,
+          headerBackVisible: true,
+        }}
+      />
+      <RootStack.Screen
+        name="Tracker"
+        component={TrackerScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+          headerBackVisible: true,
+        })}
+      />
+      {/* </RootStack.Group> */}
     </RootStack.Navigator>
   );
 };
--- src/navigation/screens/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './HomeScreen';
-export * from './NewTrackerScreen';
-export * from './TrackerScreen';
